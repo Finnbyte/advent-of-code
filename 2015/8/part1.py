61d12d656acc203d48ecf1ff2f84f0e636ff46ba
Finnbyte <eelpy@protonmail.com>
2023-07-18 15:03:43 +0300

    Update day 8 part 1 to match part 2 in implementation

--- 2015/8/part1.py
+++ 2015/8/part1.py
@@ -1,27 +1,24 @@
-def strip_escapes(s: str):
-    new_s = []
-    i = 0
-    while i < len(s):
-        match s[i]:
-            case "\"" | "\'":
-                i += 1
-            case "\\":
-                if s[i+1] != "x":
-                    new_s.append(s[i+1])
+def count_chars(s: str) -> int:
+    count = 0
+    i = 1
+    while i < len(s)-1:
+        count += 1
+        if s[i] == "\\":
+            match s[i+1]:
+                case "\"" | "\\":
                     i += 2
-                else:
-                    new_s.append("Ä")
+                case "x":
                     i += 4
-            case _:
-                new_s.append(s[i])
-                i += 1
+        else:
+            i += 1
 
-    return ''.join(new_s)
+    return count
 
 
 with open("input.txt", "r") as f:
     lines = f.readlines()
+
     raws = sum([len(s.strip()) for s in lines])
-    non_raws = sum([len(strip_escapes(s.strip())) for s in lines])
+    non_raws = sum([count_chars(s.strip()) for s in lines])
 
     print(raws - non_raws)
